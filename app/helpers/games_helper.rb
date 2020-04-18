43450023fbb961ff3a87fe7b8f08751396492dab
Michael Smith <smith11235@gmail.com>
2020-04-18 20:49:06 +0000

    half way to mobile friendly

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,13 +1,5 @@
 module GamesHelper
   def num_players
-    @num_players ||= @game.config[:players].compact.size
-  end
-
-  def board_layout
-    @board_layout ||= begin
-                        layout = YAML.load_file("config/board-layout.yml")
-                        num_players = @game.config[:players].compact.size
-                        layout[num_players]
-                      end
+    @num_players ||= @game.num_players
   end
 end
